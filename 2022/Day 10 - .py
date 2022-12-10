2b902cb7172246807154b63f9e02a27026f7a901
Ajay Ratnam <ajratnam.dev@gmail.com>
2022-12-10 18:48:14 +0530

    Day 10 Solution

--- 2022/Day 10 - .py
+++ 2022/Day 10 - .py
@@ -1,10 +1,17 @@
 with open('inputs/input10.txt', 'r') as input_file:
-	inp = input_file.read().split('\n')
+	instr = input_file.read().replace('noop', '0').replace('addx ', '').split('\n')
+
+rv = [1]
+for i, v in enumerate(map(int, instr)):
+	rv += [rv[-1], rv[-1]+v] if v else [rv[-1]]
 
 # PART 1
 print('Part 1 Answer - ', end='')
-print()
+print(sum(map(int.__mul__, rv[19::40], range(20,len(rv),40))))
 
 # PART 2
-# print('Part 2 Answer - ', end='')
-# print()
+print('Part 2 Answer - ')
+rv = [".#"[x-1<=i%40<=x+1]for i, x in enumerate(rv)]
+nrv = [rv[i*40:(i+1)*40]for i in range(len(rv)//40)]
+dup = lambda n:lambda x: sum([*zip(*[x]*n)], ())
+print(*map(''.join, dup(2)([*map(dup(4), nrv)])), sep='\n')
