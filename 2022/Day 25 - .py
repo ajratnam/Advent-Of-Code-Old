1012cb7cc9d0c5fab87dc9c8a8eb9603a364d860
Ajay Ratnam <ajratnam.dev@gmail.com>
2022-12-10 15:08:50 +0530

    Make skeleton for everyday

---
+++ 2022/Day 25 - .py
@@ -0,0 +1,10 @@
+with open('inputs/input25.txt', 'r') as input_file:
+	inp = input_file.read().split('\n')
+
+# PART 1
+print('Part 1 Answer - ', end='')
+print()
+
+# PART 2
+# print('Part 2 Answer - ', end='')
+# print()
